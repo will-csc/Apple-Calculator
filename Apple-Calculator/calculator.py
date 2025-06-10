8399e032b055579823aaec7e1180faa365baa837
William Cesar <168695261+will-csc@users.noreply.github.com>
2025-06-10 14:58:24 -0300

    Add files via upload

---
+++ Apple-Calculator/calculator.py
@@ -0,0 +1,263 @@
+from pathlib import Path
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import scrolledtext
+import re
+import math
+
+#-------------------- Caminho Base e Imagens ---------------
+BASE_PATH = Path(__file__).resolve().parent
+ASSETS_PATH = BASE_PATH / "assets"
+
+# --- Valores dos Botões ---
+button_values = ["clear","√","%","/",
+                 "7","8","9","*",
+                 "4","5","6","-",
+                 "1","2","3","+",
+                 "del","0",".","result"]
+
+# --- Rendezirar Imagens ---
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+#-------------------- Classe Calculadora ------------------
+class AppleCalculator:
+
+    # --- Inicia a calculadora ---
+    def __init__(self,width,height,bg):
+        self.width = width
+        self.height = height
+        self.bg = bg
+        self.createWindow()
+        self.placeCalculation()
+        self.placeLastHistorys()
+        self.placeResult()
+        self.insertAllButton()
+        self.history = []
+        self.window.bind("<Key>", self.keyPressed)
+        
+    # --- Cria a Janela ---
+    def createWindow(self):
+        self.window = tk.Tk()
+        self.window.geometry(f"{self.width}x{self.height}")
+        self.window.configure(bg = self.bg)
+        self.window.title("🍎 Apple Calculator by William")
+        self.createCanvas()
+    
+    # --- Cria o Canvas ---
+    def createCanvas(self):
+        self.canvas = tk.Canvas(
+            self.window,
+            bg = self.bg,
+            height = self.height,
+            width = self.width,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        
+        # Obtém o tamanho da janela
+        window_width = self.window.winfo_width()
+        window_height = self.window.winfo_height()
+
+        # Garante que os tamanhos sejam atualizados
+        self.window.update_idletasks()
+
+        # Recalcula após atualização
+        window_width = self.window.winfo_width()
+        window_height = self.window.winfo_height()
+
+        # Calcula posição para centralizar o canvas
+        x = (window_width - self.width) // 2
+        y = (window_height - self.height) // 2
+
+        self.canvas.place(x=x, y=y)
+    
+    # --- Cria área dos Cálculos ---
+    def placeCalculation(self):
+        self.calculation = tk.Text(
+            bd=0,
+            bg=self.bg,
+            fg="#BABABA",
+            highlightthickness=0,
+            font=("Helvetica", int(self.height * 0.06))
+        )
+        self.calculation.place(
+            x=18.0,
+            y=104.0,
+            width=self.width * 0.91818,
+            height=self.height * 0.1537
+        )
+        self.calculation.tag_configure("right", justify="right")
+        self.calculation.insert("1.0", "")
+        self.calculation.tag_add("right", "1.0", "end")
+    
+    # --- Cria área do Resultado ---
+    def placeResult(self):
+        self.result = tk.Text(
+            bd=0,
+            bg=self.bg,
+            fg="#FFFFFF",
+            highlightthickness=0,
+            font=("Helvetica", int(self.height * 0.09))
+        )
+        self.result.place(
+            x=18,
+            y=70.0 + self.height * 0.1537,
+            width=self.width * 0.91818,
+            height=self.height * 0.15914
+        )
+        self.result.tag_configure("right", justify="right")
+        self.result.insert("1.0", "")
+        self.result.tag_add("right", "1.0", "end")
+    
+    # --- Histórico de Calculos ---
+    def placeLastHistorys(self):
+        self.historyImage = tk.PhotoImage(
+        file=relative_to_assets("button_1.png"))
+
+        # Redimensiona usando Pillow
+        img_path = relative_to_assets("button_1.png")
+        img = Image.open(img_path)
+        resized_img = img.resize((int(self.width * 0.08522), int(self.height * 0.035303)))
+        self.historyImage = ImageTk.PhotoImage(resized_img)
+
+        historyButton = tk.Button(
+            image=self.historyImage,
+            borderwidth=0,
+            highlightthickness=0,
+            bg="black",            # <- fundo do botão
+            activebackground="black",  # <- fundo quando pressionado
+            command=lambda: self.showHistory(),
+            relief="flat"
+        )
+        historyButton.place(
+            x=37.875,
+            y=30.625118255615234,
+            width=self.width * 0.08522,
+            height=self.height * 0.035303
+        )
+    
+    # --- Inserir novo caracter ao cálculo ---
+    def addToCalculation(self,value):
+        current_text = self.calculation.get("1.0", tk.END).strip()
+        result = self.result.get("1.0", tk.END).strip()
+
+        if result and value not in ("del", "clear", "result"):
+            current_text = result + value
+            self.calculation.delete("1.0", tk.END)
+            self.calculation.insert(tk.END, current_text)
+            self.result.delete("1.0", tk.END)  # limpa o resultado antigo
+        elif value == "del":
+            current_text = current_text[:-1]
+            self.calculation.delete("1.0", tk.END)
+            self.calculation.insert(tk.END, current_text)
+        elif value == "clear":
+            self.calculation.delete("1.0", tk.END)
+            self.result.delete("1.0", tk.END)
+        elif value == "result":
+            try:
+                expression = current_text
+                expression = re.sub(r'√(\d+(\.\d+)?)', r'math.sqrt(\1)', expression)
+                expression = re.sub(r'(\d+)%', r'(\1/100)', expression)
+                result = eval(expression)
+                self.updateResult(result,current_text)
+            except Exception as e:
+                self.updateResult("Erro")
+        else:
+            self.calculation.insert(tk.END, value)
+        
+        self.calculation.tag_add("right", "1.0", "end")
+
+    # --- Atualizar o resultado ---
+    def updateResult(self,result,text):
+        self.history.append(f"{text} = {str(result)}")
+        self.calculation.delete("1.0", tk.END)
+        self.result.delete("1.0", tk.END)
+        self.result.insert(tk.END, str(result))
+        self.result.tag_add("right", "1.0", "end")
+
+    # --- Inserir Botões ---
+    def insertAllButton(self):
+        y = self.height * 0.399983
+        x = self.width * 0.022727
+
+        for i in range(20):
+            self.insertButton(i+2,button_values[i],x,y)
+
+            # Verifica se está na última coluna
+            x += self.width * 0.22045 + 5
+            if (i+1) % 4 == 0:
+                y += self.height * 0.1046 + 9
+                x = 10
+
+    # --- Inserir botão ---
+    def insertButton(self,n,value,posX,posY):
+        
+        # Redimensiona usando Pillow
+        img_path = relative_to_assets(f"button_{n}.png")
+        img = Image.open(img_path)
+        resized_img = img.resize((int(self.width * 0.22045), int(self.height * 0.1046)))
+        buttonImage = ImageTk.PhotoImage(resized_img)
+        setattr(self, f"buttonImage_{n}", buttonImage)
+
+        button = tk.Button(
+            image=buttonImage,
+            borderwidth=0,
+            highlightthickness=0,
+            bg="black",            # <- fundo do botão
+            activebackground="black",  # <- fundo quando pressionado
+            command=lambda: self.addToCalculation(value),
+            relief="flat"
+        )
+        button.place(
+            x=posX,
+            y=posY,
+            width=self.width * 0.22045,
+            height=self.height * 0.1046
+        )
+
+    # --- Mostrar histórico --
+    def showHistory(self):
+        # Cria uma nova janela (Toplevel) para o histórico
+        history_window = tk.Toplevel()
+        history_window.title("Histórico de Cálculos")
+        history_window.geometry("400x300") # Define o tamanho da janela
+
+        # Cria um widget ScrolledText (uma caixa de texto com barra de rolagem)
+        # É ideal para exibir várias linhas de texto
+        history_text_area = scrolledtext.ScrolledText(
+            history_window,
+            wrap=tk.WORD, # Quebra de linha por palavra
+            width=40,
+            height=15,
+            font=("Arial", 10)
+        )
+        history_text_area.pack(expand=True, fill="both", padx=10, pady=10)
+
+        # Insere cada item do histórico na área de texto
+        for calculo in self.history:
+            history_text_area.insert(tk.END, calculo + "\n")
+        
+        history_text_area.config(state=tk.DISABLED)
+    
+    # --- Tecla pressionada ---
+    def keyPressed(self, event):
+        char = event.char
+
+        if char in "0123456789+-*/.%√":
+            self.calculation.insert(tk.END, char)
+        elif event.keysym == "Return":
+            self.addToCalculation("result")
+        elif event.keysym == "BackSpace":
+            self.addToCalculation("del")
+        elif event.keysym == "Escape":
+            self.addToCalculation("clear")
+
+        # Reaplica o alinhamento à direita
+        self.calculation.tag_add("right", "1.0", "end")
+        
+
+if __name__ == '__main__':
+    myCalculator = AppleCalculator(400, 700, "#000000")
+    myCalculator.window.mainloop()
---
+++ Apple-Calculator/gui.py
@@ -0,0 +1,408 @@
+
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+#-------------------- Caminho Base e Imagens ---------------
+BASE_PATH = Path(__file__).resolve().parent
+ASSETS_PATH = BASE_PATH / "assets"
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+window = Tk()
+
+window.geometry("440x956")
+window.configure(bg = "#000000")
+
+
+canvas = Canvas(
+    window,
+    bg = "#000000",
+    height = 956,
+    width = 440,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    220.0,
+    177.5,
+    image=entry_image_1
+)
+entry_1 = Text(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=18.0,
+    y=104.0,
+    width=404.0,
+    height=145.0
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    220.0,
+    310.0,
+    image=entry_image_2
+)
+entry_2 = Text(
+    bd=0,
+    bg="#D9D9D9",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_2.place(
+    x=18.0,
+    y=265.0,
+    width=404.0,
+    height=88.0
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=37.875,
+    y=30.625118255615234,
+    width=37.5,
+    height=33.75005340576172
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=10.0,
+    y=369.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=10.0,
+    y=804.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=115.0,
+    y=369.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_5 = PhotoImage(
+    file=relative_to_assets("button_5.png"))
+button_5 = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_5 clicked"),
+    relief="flat"
+)
+button_5.place(
+    x=325.0,
+    y=369.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_6 = PhotoImage(
+    file=relative_to_assets("button_6.png"))
+button_6 = Button(
+    image=button_image_6,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_6 clicked"),
+    relief="flat"
+)
+button_6.place(
+    x=325.0,
+    y=803.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_7 = PhotoImage(
+    file=relative_to_assets("button_7.png"))
+button_7 = Button(
+    image=button_image_7,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_7 clicked"),
+    relief="flat"
+)
+button_7.place(
+    x=325.0,
+    y=695.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_8 = PhotoImage(
+    file=relative_to_assets("button_8.png"))
+button_8 = Button(
+    image=button_image_8,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_8 clicked"),
+    relief="flat"
+)
+button_8.place(
+    x=220.0,
+    y=803.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_9 = PhotoImage(
+    file=relative_to_assets("button_9.png"))
+button_9 = Button(
+    image=button_image_9,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_9 clicked"),
+    relief="flat"
+)
+button_9.place(
+    x=325.0,
+    y=587.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_10 = PhotoImage(
+    file=relative_to_assets("button_10.png"))
+button_10 = Button(
+    image=button_image_10,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_10 clicked"),
+    relief="flat"
+)
+button_10.place(
+    x=325.0,
+    y=478.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_11 = PhotoImage(
+    file=relative_to_assets("button_11.png"))
+button_11 = Button(
+    image=button_image_11,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_11 clicked"),
+    relief="flat"
+)
+button_11.place(
+    x=115.0,
+    y=803.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_12 = PhotoImage(
+    file=relative_to_assets("button_12.png"))
+button_12 = Button(
+    image=button_image_12,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_12 clicked"),
+    relief="flat"
+)
+button_12.place(
+    x=10.0,
+    y=695.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_13 = PhotoImage(
+    file=relative_to_assets("button_13.png"))
+button_13 = Button(
+    image=button_image_13,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_13 clicked"),
+    relief="flat"
+)
+button_13.place(
+    x=115.0,
+    y=695.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_14 = PhotoImage(
+    file=relative_to_assets("button_14.png"))
+button_14 = Button(
+    image=button_image_14,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_14 clicked"),
+    relief="flat"
+)
+button_14.place(
+    x=220.0,
+    y=695.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_15 = PhotoImage(
+    file=relative_to_assets("button_15.png"))
+button_15 = Button(
+    image=button_image_15,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_15 clicked"),
+    relief="flat"
+)
+button_15.place(
+    x=10.0,
+    y=587.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_16 = PhotoImage(
+    file=relative_to_assets("button_16.png"))
+button_16 = Button(
+    image=button_image_16,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_16 clicked"),
+    relief="flat"
+)
+button_16.place(
+    x=115.0,
+    y=587.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_17 = PhotoImage(
+    file=relative_to_assets("button_17.png"))
+button_17 = Button(
+    image=button_image_17,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_17 clicked"),
+    relief="flat"
+)
+button_17.place(
+    x=220.0,
+    y=587.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_18 = PhotoImage(
+    file=relative_to_assets("button_18.png"))
+button_18 = Button(
+    image=button_image_18,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_18 clicked"),
+    relief="flat"
+)
+button_18.place(
+    x=10.0,
+    y=478.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_19 = PhotoImage(
+    file=relative_to_assets("button_19.png"))
+button_19 = Button(
+    image=button_image_19,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_19 clicked"),
+    relief="flat"
+)
+button_19.place(
+    x=115.0,
+    y=478.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_20 = PhotoImage(
+    file=relative_to_assets("button_20.png"))
+button_20 = Button(
+    image=button_image_20,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_20 clicked"),
+    relief="flat"
+)
+button_20.place(
+    x=220.0,
+    y=478.0,
+    width=97.0,
+    height=100.0
+)
+
+button_image_21 = PhotoImage(
+    file=relative_to_assets("button_21.png"))
+button_21 = Button(
+    image=button_image_21,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_21 clicked"),
+    relief="flat"
+)
+button_21.place(
+    x=220.0,
+    y=367.0,
+    width=97.0,
+    height=100.0
+)
+window.resizable(False, False)
+window.mainloop()
